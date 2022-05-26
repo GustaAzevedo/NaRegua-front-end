ec062170c04c018cce953377f132f12df905fa68
GuilhermePCastro <guibkk@gmail.com>
2022-05-25 21:57:46 -0300

    troca dia

--- NaRegua-app/src/app/view/logado/agendamento/agendamento.component.ts
+++ NaRegua-app/src/app/view/logado/agendamento/agendamento.component.ts
@@ -11,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 export class AgendamentoComponent implements OnInit {
   agendamento: Agendamento[];
 
+  data: Date = new Date();
+  diaSemana: String;
+
   constructor(private agendamentoService: AgendamentoService) { }
 
+  retornaDia(): void{
+
+    switch(this.data.getDay()){
+      case 1:
+        this.diaSemana = 'Segunda';
+        break;
+      case 2:
+          this.diaSemana = 'Terça';
+          break;
+      case 3:
+        this.diaSemana = 'Quarta';
+        break;
+      case 4:
+        this.diaSemana = 'Quinta';
+        break;
+      case 5:
+        this.diaSemana = 'Sexta';
+        break;
+      case 6:
+        this.diaSemana = 'Sábado';
+        break;
+      case 7:
+        this.diaSemana = 'Domingo';
+        break;
+
+    }
+
+  
+
+  }
+
+
   ngOnInit(): void {
+    this.retornaDia();
     this.agendamentoService.listar().subscribe(agendamentos => {
       this.agendamento = agendamentos;
       console.log("Agendamento: " + this.agendamento[0].hr_inicio)
